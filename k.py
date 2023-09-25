e3ac72b27ecd6c9b46e325f4dda45f6fc5ca034f
jkdsdddf <145204337+jkdsdddf@users.noreply.github.com>
2023-09-25 12:37:11 +0000

    Update k.py

--- k.py
+++ k.py
@@ -1,18 +1,4 @@
-import random
-import time
-
-class RandomNumberGenerator:
-  def __init__(self, seed=None):
-    self.seed = seed
-    self.reset_seed()
-
-def reset_seed(self):
-  if self.seed is None:
-    self.seed = int(time.time())
-  random.seed(self.seed)
-
-def randint(self, a, b):
-  return random.randint(a, b)
-
-def randrange(self, start, stop=None, step=1):
-  return random.randrange(start, stop, step)
+# Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
