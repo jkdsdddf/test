b0c8cfab6cfdb5c254d544dd2765b6734d489286
jkdsdddf <145204337+jkdsdddf@users.noreply.github.com>
2023-09-28 13:24:52 +0000

    Update k.py

--- k.py
+++ k.py
@@ -2,3 +2,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Load dataset 
+data = pd.read_csv('data.csv')
+
+# Exploratory data analysis
+print(data.shape)
+print(data.describe())
