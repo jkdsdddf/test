dfcaa231256347820f8f142b84f875725aa0c384
jkdsdddf <145204337+jkdsdddf@users.noreply.github.com>
2023-09-29 10:36:36 +0000

    Update k.py

--- k.py
+++ k.py
@@ -9,3 +9,13 @@ data = pd.read_csv('data.csv')
 # Exploratory data analysis
 print(data.shape)
 print(data.describe())
+
+# Visualize data
+plt.hist(data['column1'], bins = 20)
+plt.boxplot(data['column2'])
+plt.scatter(data['column1'], data['column2'])
+plt.show()
+
+# Preprocess data
+# Handle missing values
+data = data.fillna(method='ffill')
